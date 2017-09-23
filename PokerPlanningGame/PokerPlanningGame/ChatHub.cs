48491920aeecf1d743353a0c82e5d21f6fc53a24
aciuca <alingabriel.ciuca@gmail.com>
2017-09-23 23:30:15 +0300

    added user displaying

--- PokerPlanningGame/PokerPlanningGame/ChatHub.cs
+++ PokerPlanningGame/PokerPlanningGame/ChatHub.cs
@@ -1,21 +1,48 @@
 ﻿using System;
+using Microsoft.AspNet.SignalR;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using static PokerPlanningGame.UserHandler;
 
 namespace PokerPlanningGame
 {
+    public static class UserHandler
+    {
+        public static readonly Dictionary<string, string> UsersDictionary = new Dictionary<string, string>();
+    }
+
+    public static class MessagesHandler
+    {
+        public static readonly Dictionary<Guid, string> MessagesList = new Dictionary<Guid, string>();
+    }
+
     public class ChatHub : Hub
     {
+        public void RegisterUser(string name)
+        {
+            if (UsersDictionary.ContainsKey(Context.ConnectionId))
+            {
+                return;
+            }
+            UsersDictionary.Add(Context.ConnectionId, name);
+            Clients.All.updateUserNamesList(UsersDictionary.Select(p =>
+            new {value = p.Value}).ToList());
+        }
+
+
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
         }
 
-        public void GetConnectedClients()
+        public override Task OnDisconnected(bool stopCalled)
         {
+            UsersDictionary.Remove(Context.ConnectionId);
+            Clients.All.updateUserNamesList(UsersDictionary.Select(p =>
+                new { value = p.Value }).ToList());
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
---
+++ PokerPlanningGame/PokerPlanningGame/Scripts/app.js
@@ -0,0 +1,54 @@
+﻿$(function () {
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatHub;
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message.
+        var encodedName = $('<div />').text(name).html();
+        var encodedMsg = $('<div />').text(message).html();
+        // Add the message to the page.
+        $('#discussion').append('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+
+    chat.client.showNumberOfConnectedUsers = function (count) {
+        var encodedCount = $('<div />').text(count).html();
+        $('#connectedUsers').val('');
+        $('#connectedUsers').append('<span>' + encodedCount + '</span>');
+    }
+
+    chat.client.updateUserNamesList = function (userNames) {
+        $('#userNames').empty();
+        if (userNames && userNames.length > 0) {
+            for (var i = 0; i < userNames.length; i++) {
+                $('#userNames').append('<li>' + userNames[i].value + '</li>');
+            }
+        }
+
+    }
+
+    // Get the user name and store it to prepend to messages.
+    $('#displayName').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+    //$('#message').focus();
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        var init = function (userName) {
+            chat.server.registerUser(userName)
+                .then(
+                function (data) {
+
+                }, function () {
+
+                });
+        }
+        init($('#displayName').val());
+
+        $('#sendMessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayName').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+});
